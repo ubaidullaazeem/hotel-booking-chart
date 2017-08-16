cc749c2e184490e6227cb389000a031b43059cd5
kumar <kumar@riverstonetech.com>
2017-08-16 12:05:57 +0530

    Fixed some of the lint errors

--- modules/core/client/config/core.client.colours.js
+++ modules/core/client/config/core.client.colours.js
@@ -1,55 +1,54 @@
-(function () {
+(function() {
 	'use strict';
 	angular
-	.module('core')
-	.constant('COLOURS', [
-						{
-							name : 'Black', code : '#000000'
-						},
-						{
-							name : 'Blue', code : '#0000FF'
-						},
-						{
-							name : 'Brown', code : '#A52A2A'
-						},
-						{
-							name : 'Gold', code : '#FFD700'
-						},
-						{
-							name : 'Gray', code : '#999999'
-						},
-						{
-							name : 'Green', code : '#4caf50'
-						},
-						{
-							name : 'Lime', code : '#00FF00'
-						},
-						{
-							name : 'Orange', code : '#ff9800'
-						},
-						{
-							name : 'Pink', code : '#e91e63'
-						},
-						{
-							name : 'Purple', code : '#800080'
-						},
-						{
-							name : 'Red', code : '#f44336'
-						},
-						{
-							name : 'Silver', code : '#C0C0C0'
-						},
-						{
-							name : 'Violet', code : '#EE82EE'
-						},
-						{
-							name : 'White', code : '#FFFFFF'
-						},
-						{
-							name : 'Yellow', code : '#FFFF00'
-						},
-						{
-							name : 'Azure', code : '#00bcd4'
-						}
-						]);
+		.module('core')
+		.constant('COLOURS', [{
+			name: 'Black',
+			code: '#000000'
+		}, {
+			name: 'Blue',
+			code: '#0000FF'
+		}, {
+			name: 'Brown',
+			code: '#A52A2A'
+		}, {
+			name: 'Gold',
+			code: '#FFD700'
+		}, {
+			name: 'Gray',
+			code: '#999999'
+		}, {
+			name: 'Green',
+			code: '#4caf50'
+		}, {
+			name: 'Lime',
+			code: '#00FF00'
+		}, {
+			name: 'Orange',
+			code: '#ff9800'
+		}, {
+			name: 'Pink',
+			code: '#e91e63'
+		}, {
+			name: 'Purple',
+			code: '#800080'
+		}, {
+			name: 'Red',
+			code: '#f44336'
+		}, {
+			name: 'Silver',
+			code: '#C0C0C0'
+		}, {
+			name: 'Violet',
+			code: '#EE82EE'
+		}, {
+			name: 'White',
+			code: '#FFFFFF'
+		}, {
+			name: 'Yellow',
+			code: '#FFFF00'
+		}, {
+			name: 'Azure',
+			code: '#00bcd4'
+		}]);
 }).call(this);
