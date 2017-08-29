49b04cebff0d5604def77fc687d719655fc0a4f6
kumar <kumar@riverstonetech.com>
2017-08-29 11:58:24 +0530

    updated invoice image width

--- modules/newbookings/client/controllers/newbookings.client.controller.js
+++ modules/newbookings/client/controllers/newbookings.client.controller.js
@@ -374,7 +374,7 @@
                   '<table width="100%" style="border-collapse: collapse; border: 1px solid black; table-layout: fixed;" cellspacing="0" cellpadding="0"> <tbody>'+
                   '<tr style="border-bottom: 1px solid black; text-align:center;"><td width="20%"></td><td width="45%">INVOICE</td><td width="35%"></td></tr>'+
                   '<tr style="border-bottom: 1px solid black; text-align:left;">'+
-                    '<td width="20%"><img style="width: 140px;" src="' + baseUrl + '/modules/core/client/img/logo-bw.png"/></td>'+
+                    '<td width="20%"><img style="width: 110px;" src="' + baseUrl + '/modules/core/client/img/logo-bw.png"/></td>'+
                     '<td width="80%" colspan="2" >Dev&apos;s Ark, Second Floor, AD-79&80, 5th Avenue<br/> Anna Nagar, Chennai - 600 040<br/> Phone Nos : 044-45552479 / 044-26222479<br/> GSTIN No. 33AAFPJ8706K1ZM</td>'+                    
                   '</tr>'+
                   '<tr style="border-bottom: 1px solid black;">'+
