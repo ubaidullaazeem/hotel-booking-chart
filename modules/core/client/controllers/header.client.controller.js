dcc849e4f8bbb9beb6e96e15a660f36f657f3972
kumar <kumar@riverstonetech.com>
2017-09-08 13:28:13 +0530

    Implemented expanding only one menu at a time.

--- modules/core/client/controllers/header.client.controller.js
+++ modules/core/client/controllers/header.client.controller.js
@@ -58,5 +58,18 @@
        //gapi.auth2.getAuthInstance().disconnect();
     };
 
+    // For expanding only one menu at a time.
+    $('.nav').on('show.bs.collapse', function() {
+      var actives = $(this).find('.collapse.in'),
+        hasData;
+
+      if (actives && actives.length) {
+        hasData = actives.data('collapse')
+        if (hasData && hasData.transitioning) return
+        actives.collapse('hide')
+        hasData || actives.data('collapse', null)
+      }
+    });
+
   }
 }());
